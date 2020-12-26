761ea855a9f7214cb361dead78d0e1d3aa0fb182
thepanshuyadav <thepanshuyadav@gmail.com>
2020-12-26 12:55:43 +0530

    added splash screen and icons

---
+++ Android/Diakart/app/src/main/java/com/thepanshu/diakart/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.thepanshu.diakart
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.*
+import kotlin.concurrent.schedule
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val i = Intent(this, MainActivity::class.java)
+
+        Timer("SettingUp", false).schedule(3000) {
+            startActivity(i)
+        }
+
+    }
+}
