3db94e0ae23df57adcd0b1c7e31f3587dd9d0a43
thepanshuyadav <thepanshuyadav@gmail.com>
2021-01-06 22:54:47 +0530

    rating click listener implemented

--- Android/Diakart/app/src/main/java/com/thepanshu/diakart/ui/ProductDetailFragment.kt
+++ Android/Diakart/app/src/main/java/com/thepanshu/diakart/ui/ProductDetailFragment.kt
@@ -1,14 +1,15 @@
 package com.thepanshu.diakart.ui
 
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
+import android.widget.*
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.thepanshu.diakart.R
@@ -18,9 +19,16 @@ import com.thepanshu.diakart.models.Product
 class ProductDetailFragment : Fragment() {
     private lateinit var product: Product
     private lateinit var intentToSite: Intent
+
+    //////Ratings Layout
+    private lateinit var rateNowLayout: LinearLayout
+    /////
+    companion object {
+        var rating = 0
+    }
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val rootView = inflater.inflate(R.layout.fragment_product_detail, container, false)
@@ -28,6 +36,19 @@ class ProductDetailFragment : Fragment() {
         rvProductImage.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
         rvProductImage.adapter = ImageListAdapter(product.product_images)
         return rootView
+    }
+
+    private fun setRating() {
+        for(i in 0..4) {
+            val starImageView = rateNowLayout.getChildAt(i) as ImageView
+            //starImageView.imageTintList = ColorStateList.valueOf(Color.parseColor("#545455"))
+            if(i <= rating) {
+                starImageView.imageTintList = ColorStateList.valueOf(Color.parseColor("#FFFFEA00"))
+            }
+            else {
+                starImageView.imageTintList = ColorStateList.valueOf(Color.parseColor("#545455"))
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,6 +66,20 @@ class ProductDetailFragment : Fragment() {
 
         view.findViewById<TextView>(R.id.specific_product_price).text = product.price.toString()
         view.findViewById<TextView>(R.id.specific_product_quantity).text = " (${product.quantity})"
+
+        rateNowLayout = view.findViewById(R.id.rate_now_container)
+        rateNowLayout.setOnClickListener {
+            Toast.makeText(context, rateNowLayout.childCount.toString(), Toast.LENGTH_SHORT).show()
+        }
+        val childCount: Int = rateNowLayout.childCount
+        for (i in 0 until childCount) {
+            val v: View = rateNowLayout.getChildAt(i)
+            v.setOnClickListener {
+                rating = i
+                setRating()
+            }
+        }
+
         intentToSite = Intent(Intent.ACTION_VIEW, Uri.parse(product.link))
         val buyButton: Button = view.findViewById<TextView>(R.id.link_external_btn) as Button
 
@@ -54,7 +89,5 @@ class ProductDetailFragment : Fragment() {
             }
 
         }
-
-
     }
 }
