8a7fa4186f5c00646e7dfdfb441ab2ecfffdf6de
thepanshuyadav <thepanshuyadav@gmail.com>
2021-01-22 14:44:27 +0530

    fixed progress bar

--- Android/Diakart/app/src/main/java/com/thepanshu/diakart/ui/account/AccountFragment.kt
+++ Android/Diakart/app/src/main/java/com/thepanshu/diakart/ui/account/AccountFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
@@ -21,7 +22,7 @@ import com.thepanshu.diakart.authenticate.RegisterActivity
 import com.thepanshu.diakart.models.UserModel
 
 
-class AccountFragment : Fragment(), GridProductAdapter.OnCategoryGridProductClickListener {
+class AccountFragment : Fragment() {
 
     companion object {
         fun newInstance() = AccountFragment()
@@ -30,33 +31,38 @@ class AccountFragment : Fragment(), GridProductAdapter.OnCategoryGridProductClic
     private lateinit var viewModel: AccountViewModel
     private lateinit  var user:UserModel
 
+    private lateinit var usernameTv: TextView
+    private lateinit var userEmailTv: TextView
+    private lateinit var profileImage: ImageView
+
+    private lateinit var progressBar:ProgressBar
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         var root = inflater.inflate(R.layout.account_fragment, container, false)
+
+        usernameTv = root.findViewById(R.id.username_tv)
+        userEmailTv = root.findViewById(R.id.user_email_tv)
+        profileImage = root.findViewById(R.id.profile_pic_img)
+
+        progressBar = root.findViewById(R.id.progressBar)
+
+        return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(AccountViewModel::class.java)
 
-//        val wishListGrid = root.findViewById<TextView>(R.id.grid_category_title)
-//        wishListGrid.text = "My Wish List"
-//        val openWishListButton = root.findViewById<Button>(R.id.view_all_btn)
-//        openWishListButton.setOnClickListener {
-//            val navController = view?.let { Navigation.findNavController(it) }
-//            navController?.navigate(R.id.action_nav_account_to_nav_wishlist)
-//        }
-        val usernameTv = root.findViewById<TextView>(R.id.username_tv)
-        val userEmailTv = root.findViewById<TextView>(R.id.user_email_tv)
-        val profileImage = root.findViewById<ImageView>(R.id.profile_pic_img)
         viewModel.getUserDetail().observe(viewLifecycleOwner, Observer<UserModel> {
-            //progressBar.visibility = View.VISIBLE
+            progressBar.visibility = View.VISIBLE
             user = it
             usernameTv.text = user.name
             userEmailTv.text = user.email
             Picasso.get().load(user.profile_pic).into(profileImage)
-            //progressBar.visibility = View.GONE
+            progressBar.visibility = View.GONE
         })
-        //        val rvGrid = root.findViewById<RecyclerView>(R.id.home_category_grid_view)
-//        rvGrid.layoutManager = GridLayoutManager(activity, 1, GridLayoutManager.HORIZONTAL, false)
-//        rvGrid.adapter = GridProductAdapter(gridCategoryList, this)
-        return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -67,16 +73,6 @@ class AccountFragment : Fragment(), GridProductAdapter.OnCategoryGridProductClic
             startActivity(Intent(activity, RegisterActivity::class.java))
             requireActivity().finish()
         }
-    }
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-
-    override fun onCategoryGridProductClick(gridIdx: Int, position: Int) {
-        //TODO("Not yet implemented")
     }
 
 }
